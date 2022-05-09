8f931ab61ac0cc776669f4fda6336fac6e050ecb
linghaosong <linghao.song@duke.edu>
2022-05-09 10:00:48 -0700

    Delete matrices directory

--- matrices/download.sh
+++
@@ -1,5 +0,0 @@
-wget https://suitesparse-collection-website.herokuapp.com/MM/PARSEC/Si41Ge41H72.tar.gz
-wget https://suitesparse-collection-website.herokuapp.com/MM/GHS_psdef/crankseg_2.tar.gz
-tar -zxvf Si41Ge41H72.tar.gz
-tar -zxvf crankseg_2.tar.gz
-rm *.tar.gz
