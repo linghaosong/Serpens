d26b44d0c2b437eb5c94f7c90c3eb7ca97851b73
linghao.song <linghao.song@duke.edu>
2022-05-09 09:38:00 -0700

    example suitesparse matrices

---
+++ matrices/download.sh
@@ -0,0 +1,4 @@
+wget https://suitesparse-collection-website.herokuapp.com/MM/PARSEC/Si41Ge41H72.tar.gz
+wget https://suitesparse-collection-website.herokuapp.com/MM/GHS_psdef/crankseg_2.tar.gz
+tar -zxvf *.tar.gz
+rm *.tar.gz
