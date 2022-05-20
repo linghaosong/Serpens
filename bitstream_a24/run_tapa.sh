cc05d8a29af67b1e3dfa30704087e81b900f0ebb
linghaosong <linghaosong@cs.ucla.edu>
2022-05-20 16:22:17 -0700

    a24 dse tapac

---
+++ bitstream_a24/run_tapa.sh
@@ -0,0 +1,22 @@
+tapac \
+  --work-dir run \
+  --top Serpens \
+  --part-num xcu280-fsvh2892-2L-e \
+  --platform xilinx_u280_xdma_201920_3 \
+  --clock-period 3.33 \
+  -o Serpens.xo \
+  --constraint Serpens_floorplan.tcl \
+  --connectivity ../link_config_a24.ini \
+  --read-only-args edge_list_ptr \
+  --read-only-args edge_list_ch* \
+  --read-only-args vec_X \
+  --read-only-args vec_Y \
+  --write-only-args vec_Y_out \
+  --enable-synth-util \
+  --max-parallel-synth-jobs 16 \
+  --enable-hbm-binding-adjustment \
+  --run-floorplan-dse \
+  --min-area-limit 0.6 \
+  --min-slr-width-limit 5000 \
+  ../src/serpens.cpp \
+   2>&1 | tee tapa.log
